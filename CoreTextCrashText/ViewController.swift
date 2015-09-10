28919b3c2710ed321052956010bb5a96a896217e
Ryota Iwai <iwai_ryota_gn@cyberagent.co.jp>
2015-09-10 11:26:35 +0900

    Fist commit

---
+++ CoreTextCrashText/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CoreTextCrashText
+//
+//  Created by Ryota Iwai on 2015/09/09.
+//  Copyright (c) 2015年 Ryota Iwai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.label.text = "( °◡͐︎°)"
+    }
+
+}
+
